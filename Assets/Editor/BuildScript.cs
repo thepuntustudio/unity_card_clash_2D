6ad79891b3f9a2061d53e6d228cede41472cd5d1
Subodh668 <smsb668@gmail.com>
2026-08-31 01:08:08 +0545

    Update project assets and settings
    - Modified MorrisRoman-Black SDF font asset to include a new GUID and enabled font features.
    - Deleted unused Knight_male.png asset.
    - Added new Icons folder with multiple icon assets including CardClashIcon, Puntu_Studio, and Sword_cursor.
    - Updated Universal Render Pipeline settings with new runtime settings.
    - Cleaned up manifest.json by removing unused dependencies.
    - Changed company name to PuntuStudio and updated splash screen settings in ProjectSettings.
    - Enabled Unity Connect settings for improved analytics.

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -3,13 +3,21 @@ using System.IO;
 
 public class BuildScript
 {
+    static string[] GetScenes()
+    {
+        var scenes = EditorBuildSettings.scenes;
+        var paths = new string[scenes.Length];
+        for (int i = 0; i < scenes.Length; i++) paths[i] = scenes[i].path;
+        return paths;
+    }
+    
     [MenuItem("Build/Windows")]
     static void BuildWindows()
     {
         BuildPlayerOptions options = new BuildPlayerOptions();
         options.target = BuildTarget.StandaloneWindows64;
-        options.locationPathName = "Builds/Windows/CardBattler.exe";
-        options.scenes = new[] { "Assets/Scenes/MainScene.unity" };
+        options.locationPathName = "Builds/Windows/CardClash.exe";
+        options.scenes = GetScenes();
         BuildPipeline.BuildPlayer(options);
     }
 
@@ -18,8 +26,8 @@ public class BuildScript
     {
         BuildPlayerOptions options = new BuildPlayerOptions();
         options.target = BuildTarget.StandaloneOSX;
-        options.locationPathName = "Builds/Mac/CardBattler.app";
-        options.scenes = new[] { "Assets/Scenes/MainScene.unity" };
+        options.locationPathName = "Builds/Mac/CardClash.app";
+        options.scenes = GetScenes();
         BuildPipeline.BuildPlayer(options);
     }
 
@@ -29,7 +37,7 @@ public class BuildScript
         BuildPlayerOptions options = new BuildPlayerOptions();
         options.target = BuildTarget.WebGL;
         options.locationPathName = "Builds/WebGL";
-        options.scenes = new[] { "Assets/Scenes/MainScene.unity" };
+        options.scenes = GetScenes();
         BuildPipeline.BuildPlayer(options);
     }
 
@@ -38,8 +46,8 @@ public class BuildScript
     {
         BuildPlayerOptions options = new BuildPlayerOptions();
         options.target = BuildTarget.Android;
-        options.locationPathName = "Builds/Android/CardBattler.apk";
-        options.scenes = new[] { "Assets/Scenes/MainScene.unity" };
+        options.locationPathName = "Builds/Android/CardClash.apk";
+        options.scenes = GetScenes();
         BuildPipeline.BuildPlayer(options);
     }
 }
